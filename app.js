12f23aa6d0dd5cc35d8f97038615f3c4f28269b0
Barry John Williams <barry@bjw.me.uk>
2014-05-05 22:25:24 +0100

    Reindent and converted tabs to spaces

--- app.js
+++ app.js
@@ -7,22 +7,35 @@ var storage = require('node-persist');
 var manager = new DeviceManager();
 var app = express();
 
+storage.initSync();
+
+if (!storage.getItem('schedules.json')) {
+    storage.setItem('schedules.json', []);
+}
+
+var schedulesFor = function (uuid) {
+    return _.filter(storage.getItem('schedules.json'), function(schedule) {
+        return schedule.uuid === uuid;
+    });
+};
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
 app.get('/', function(req, res) {
-	var devices = _.map(manager.getDevices(), function (uuid) { 
-		var device = manager.getDevice(uuid); 
-		return {
-			uuid: uuid,
-			icon: device.icon,
-			name: device.name
-		}
-	});
-	res.render('index', { 
-		title : 'Devices',
-		devices: devices
-	} );
+    var devices = _.map(manager.getDevices(), function (uuid) { 
+        var device = manager.getDevice(uuid); 
+        return {
+            uuid: uuid,
+            icon: device.icon,
+            name: device.name,
+            schedules: schedulesFor(uuid)
+        }
+    });
+    res.render('index', { 
+        title : 'Devices',
+        devices: devices
+    } );
 });
 
 var server = app.listen(18080);
@@ -30,22 +43,16 @@ var server = app.listen(18080);
 var changeSource= function (uuid, sourceId) {
     return function () {
         var device = manager.getDevice(uuid);
-	if (device) {
-            manager.changeSource(device, sourceId);
-	}
+        if (device) {
+                manager.changeSource(device, sourceId);
+        }
     }
 };
 
-storage.initSync();
-
-if (!storage.getItem('schedules.json')) {
-    storage.setItem('schedules.json', []);
-}
-
 _.each(storage.getItem('schedules.json'), function (schedule) {
-	var recurrence = new scheduler.RecurrenceRule();
-	recurrence.dayOfWeek = schedule.wakeUp.dayOfWeek;
-	recurrence.hour = schedule.wakeUp.hour;
-	recurrence.minute = schedule.wakeUp.minute;
-	scheduler.scheduleJob(recurrence, changeSource(schedule.uuid,schedule.source));
+    var recurrence = new scheduler.RecurrenceRule();
+    recurrence.dayOfWeek = schedule.wakeUp.dayOfWeek;
+    recurrence.hour = schedule.wakeUp.hour;
+    recurrence.minute = schedule.wakeUp.minute;
+    scheduler.scheduleJob(recurrence, changeSource(schedule.uuid,schedule.source));
 });
