b8ddd8ea1549143be99232d922bb15569c732492
Barry John Williams <barry@bjw.me.uk>
2015-01-04 00:45:14 +0000

    Scheduled playlist start is now a drop down with suggestions

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,91 +2,96 @@
 
 /* Controllers */
 
-var getRefresh = function (scope, http) {
+function getRefresh(scope, http) {
     return function () {
         http({
-          method: 'GET',
-          url: '/api/devices'
-        }).
-        success(function (data, status, headers, config) {
+            method: 'GET',
+            url: '/api/devices'
+        }).success(function (data, status, headers, config) {
           scope.devices = data;
         });
-    }
+        http({
+            method: 'GET',
+            url: '/api/playlists'
+        }).success(function (data, status, headers, config) {
+            scope.playlists = data;
+        });
+    };
 };
 
 var refreshAll;
 
 angular.module('upnpControllers', [])
-	.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+    .controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         refreshAll = getRefresh($scope, $http);
         refreshAll();
-	}])
-	.controller('DeviceCtrl', ['$scope', '$http', function ($scope, $http) {
-		var dsStorePlaylist = _.findWhere($scope.device.links, { 'rel' : 'store-playlist' });
-		var dsReplacePlaylist = _.findWhere($scope.device.links, { 'rel' : 'replace-playlist' });
-		$scope['storePlaylist'] = function (playlistName) {
-			if (dsStorePlaylist) {
-				$http({
-					method: 'PUT',
-					url: dsStorePlaylist.href + playlistName
-				});
-			}
-		};
-		$scope['replacePlaylist'] = function(playlistName) {
-			if (dsReplacePlaylist) {
-				$http({
-					method: 'POST',
-					url: dsReplacePlaylist.href,
-					data: { playlistName: playlistName }
-				});
-			}
-		}
-		$scope['newSchedule'] = {
-			days: {
-				'mon' : false,
-				'tue' : false,
-				'wed' : false,
-				'thu' : false,
-				'fri' : false,
-				'sat' : false,
-				'sun' : false
-			},
-			playlistName: '',
-			action: 'wake',
-			time: ''
-		};
-		$scope['toggleDay'] = function (dayOfWeek) {
-			if ($scope['newSchedule'].days[dayOfWeek] !== void 0) {
-				$scope['newSchedule'].days[dayOfWeek] = !$scope['newSchedule'].days[dayOfWeek];
-			}
-		};
-		$scope['addWakeUp'] = function (newSchedule) {
-			var result = _.findWhere($scope.device.links, { 'rel' : 'add-schedule' });
-			var body = {
-				days : newSchedule.days,
-				time: newSchedule.time,
-				action: newSchedule.action,
-				playlistName: newSchedule.playlistName
-			};
-			$http({
-				method: 'POST',
-				url: result.href,
-				data: body
-			})
+    }])
+    .controller('DeviceCtrl', ['$scope', '$http', function ($scope, $http) {
+        var dsStorePlaylist = _.findWhere($scope.device.links, { 'rel' : 'store-playlist' });
+        var dsReplacePlaylist = _.findWhere($scope.device.links, { 'rel' : 'replace-playlist' });
+        $scope['storePlaylist'] = function (playlistName) {
+            if (dsStorePlaylist) {
+                $http({
+                    method: 'PUT',
+                    url: dsStorePlaylist.href + playlistName
+                });
+            }
+        };
+        $scope['replacePlaylist'] = function(playlistName) {
+            if (dsReplacePlaylist) {
+                $http({
+                    method: 'POST',
+                    url: dsReplacePlaylist.href,
+                    data: { playlistName: playlistName }
+                });
+            }
+        }
+        $scope['newSchedule'] = {
+            days: {
+                'mon' : false,
+                'tue' : false,
+                'wed' : false,
+                'thu' : false,
+                'fri' : false,
+                'sat' : false,
+                'sun' : false
+            },
+            playlistName: '',
+            action: 'wake',
+            time: ''
+        };
+        $scope['toggleDay'] = function (dayOfWeek) {
+            if ($scope['newSchedule'].days[dayOfWeek] !== void 0) {
+                $scope['newSchedule'].days[dayOfWeek] = !$scope['newSchedule'].days[dayOfWeek];
+            }
+        };
+        $scope['addWakeUp'] = function (newSchedule) {
+            var result = _.findWhere($scope.device.links, { 'rel' : 'add-schedule' });
+            var body = {
+                days : newSchedule.days,
+                time: newSchedule.time,
+                action: newSchedule.action,
+                playlistName: newSchedule.playlistName
+            };
+            $http({
+                method: 'POST',
+                url: result.href,
+                data: body
+            })
             .success(function () {
                 refreshAll();
             });
-		};
-		$scope['deleteWakeUp'] = function (schedule) {
-			var result = _.findWhere(schedule.links, { 'rel' : 'delete' });
-			if (result) {
-				$http({
-					method: 'DELETE',
-					url: result.href
-				})
+        };
+        $scope['deleteWakeUp'] = function (schedule) {
+            var result = _.findWhere(schedule.links, { 'rel' : 'delete' });
+            if (result) {
+                $http({
+                    method: 'DELETE',
+                    url: result.href
+                })
                 .success(function () {
                     refreshAll();
                 });
-			}
-		};
-	}]);
+            }
+        };
+    }]);
