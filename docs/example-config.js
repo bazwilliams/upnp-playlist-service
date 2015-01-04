9a025a5e978e5fb89f6cdf44395a501f3c1a79c6
Barry John Williams <barry@bjw.me.uk>
2015-01-04 01:55:40 +0000

    Added example config.js file

---
+++ docs/example-config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+// Unix locations
+//config.musicRoot = '/mnt/media/music';
+//config.playlistPath = '/mnt/media/music/Playlists';
+
+// Windows locations
+config.musicRoot = 'C:\\Users\\barry\\Dropbox';
+config.playlistPath = 'C:\\Users\\barry\\Dropbox\\playlists\\';
+
+module.exports = config;
