62b08594f3cd39baf77161fc701fe45eb7779a72
Barry John Williams <barry@bjw.me.uk>
2015-01-21 19:04:59 +0000

    Refactored recipes

--- server/recipes.js
+++ server/recipes.js
@@ -6,16 +6,32 @@ function delay(milliseconds) {
         setTimeout(callback, milliseconds);
     };
 }
+function ensureOn(ds) {
+    return function checkStandbyState(callback) {
+        async.waterfall([
+            ds.standbyState,
+            function switchOnIfOff(currentStandbyState, iterCallback) {
+                if (currentStandbyState === '1') {
+                    ds.powerOn(function(err, results) {
+                        delay(500);
+                        callback();
+                    });
+                } else {
+                    callback();
+                }
+            }
+        ], callback);
+    }
+}
 exports.play = function play(ds, playlistName, shuffle, callback) {
     if (playlistName) {
         async.waterfall([
-            ds.powerOn,
-            delay(500),
+            ensureOn(ds),
             shuffle ? ds.enableShuffle : ds.disableShuffle,
-            function(iterCallback) {
+            function replacePlaylist(iterCallback) {
                 playlists.replacePlaylist(ds, playlistName, iterCallback);
             },
-            function(trackIds, iterCallback) {
+            function startPlayback(trackIds, iterCallback) {
                 if (shuffle) {
                     ds.playFromPlaylistIndex(Math.floor((Math.random() * trackIds.length) + 1), iterCallback);
                 } else {
@@ -24,10 +40,9 @@ exports.play = function play(ds, playlistName, shuffle, callback) {
             }
         ], callback);
     } else {
-        async.series([
-            ds.powerOn,
-            delay(500),
-            function(iterCallback) {
+        async.waterfall([
+            ensureOn(ds),
+            function changeSourceToRadio(iterCallback) {
                 ds.changeSource(1, iterCallback);
             },
             delay(1000),
