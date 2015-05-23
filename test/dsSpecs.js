2094d46aa27baa20b0f4b8fd754961839a2b687b
Barry John Williams <barry@bjw.me.uk>
2015-05-23 01:54:35 +0100

    Another test added for radio station parsing

--- test/dsSpecs.js
+++ test/dsSpecs.js
@@ -28,11 +28,50 @@ describe('Ds', function () {
         mockery.registerMock('./responseparsers.js', responseParserMock);
         
         var Ds = require('../server/ds.js').Ds;
-        ds = new Ds('/test', {'urn:av-openhome-org:service:Info:1' : '/control' });
+        ds = new Ds('/test', {
+            'urn:av-openhome-org:service:Info:1' : '/control',
+            'urn:av-openhome-org:service:Radio:1' : '/control'
+        });
     });
     afterEach(function () {
         mockery.deregisterAll();
         mockery.disable();
+    });
+    describe('When getting list of radio stations', function () {
+        var radioStations;
+        beforeEach(function (done) {
+            soapObject = { 
+                's:Envelope': { 
+                    's:Body' : { 
+                        'u:ReadListResponse' : {
+                            'ChannelList' : '<ChannelList><Entry><Id>23</Id><Metadata>&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;&quot; parentID=&quot;&quot; restricted=&quot;True&quot;&gt;&lt;dc:title&gt;BBC Radio 6 Music (AAA)&lt;/dc:title&gt;&lt;res protocolInfo=&quot;*:*:*:*&quot; bitrate=&quot;40000&quot;&gt;http://opml.radiotime.com/Tune.ashx?id=s44491&amp;amp;formats=mp3,wma,aac,wmvideo,ogg,hls&amp;amp;partnerId=ah2rjr68&amp;amp;username=bazwilliams&amp;amp;c=ebrowse&lt;/res&gt;&lt;upnp:albumArtURI&gt;http://cdn-radiotime-logos.tunein.com/s44491g.png&lt;/upnp:albumArtURI&gt;&lt;upnp:class&gt;object.item.audioItem&lt;/upnp:class&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;</Metadata></Entry></ChannelList>'
+                        }
+                    }
+                }
+            };
+            ds.retrieveRadioStationDetails('23 34', function (err, data) {
+                radioStations = data;
+                done();
+            });
+            soapRequestCb({
+                setEncoding: sinon.spy()
+            });
+        });
+        it('Radio station list should be an array with 1 item', function () {
+            expect(radioStations).to.be.an('array').and.have.length(1);
+        });
+        it('Radio station list Id to be 23', function () {
+            expect(radioStations[0].id).to.be.eql('23');
+        });
+        it('Radio station list uri to be correct', function () {
+            expect(radioStations[0].uri).to.be.eql('http://opml.radiotime.com/Tune.ashx?id=s44491&formats=mp3,wma,aac,wmvideo,ogg,hls&partnerId=ah2rjr68&username=bazwilliams&c=ebrowse');
+        });
+        it('Radio station list title to be BBC Radio 6 Music (AAA)', function () {
+            expect(radioStations[0].title).to.be.eql('BBC Radio 6 Music (AAA)');
+        });
+        it('Radio station list artwork to be correct', function () {
+            expect(radioStations[0].artwork).to.be.eql('http://cdn-radiotime-logos.tunein.com/s44491g.png');
+        });
     });
     describe('When getting current track details', function() {
         var trackDetails;
