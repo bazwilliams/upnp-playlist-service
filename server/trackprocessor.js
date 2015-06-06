37e501cc114e508dc0cc97a148ffd5b26d2b67e9
Barry John Williams <barry@bjw.me.uk>
2015-06-07 00:06:48 +0100

    Added test for track processor - fixed bug for non minimserver http URIs

--- server/trackprocessor.js
+++ server/trackprocessor.js
@@ -1,8 +1,10 @@
+"use strict";
+
 var configManager = require('./configmanager.js');
 var path = require('path');
 
 exports.translate = function(uri) {
-    if (uri.indexOf('http:') === 0) {
+    if (uri.indexOf('http:') === 0 && uri.indexOf('minimserver') > 0) {
         return path.join(configManager.config().musicRoot, decodeURIComponent(uri.replace(/http:.*\/minimserver\/\*\/[^\/.]*\//, '').replace(/\*/g, '%')));
     } else {
         return uri;
